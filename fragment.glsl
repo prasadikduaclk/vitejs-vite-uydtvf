07bfe2aef591e013fc6c79c8f1c2b96c73611db5
prasadikduaclk <115408126+prasadikduaclk@users.noreply.github.com>
2022-10-24 18:10:14 +0530

    1 file modified, 2 deleted

--- fragment.glsl
+++
@@ -1,6 +0,0 @@
-uniform sampler2D globeTexture;
-varying vec2 vertexUV;
-
-void main(){
-  gl_FragColor = texture2D(globeTexture,vertexUV);
-}
--- main.js
+++ main.js
@@ -3,8 +3,8 @@
 //import { setupCounter } from './counter.js'
 
 import * as THREE from "https://cdn.skypack.dev/three@0.145.0"
-import vertexShader from '/vertex.glsl'
-import fragmentShader from '/fragment.glsl'
+//import vertexShader from '/vertex.glsl'
+//import fragmentShader from '/fragment.glsl'
 //console.log(fragmentShader)
 
 
@@ -27,8 +27,8 @@ document.body.appendChild (renderer.domElement)
 //create a sphere
 const sphere = new THREE.Mesh(
   new THREE.SphereGeometry(5, 50, 50),
-  //new THREE.MeshBasicMaterial({
-  new THREE.ShaderMaterial({
+  new THREE.MeshBasicMaterial({
+ /* new THREE.ShaderMaterial({
     //color: 0xff0000,
     vertexShader,
     fragmentShader,
@@ -37,8 +37,8 @@ const sphere = new THREE.Mesh(
         value: new TextureLoader().load('/globe.jpg')
 
       }
-    }
-   //map : new THREE.TextureLoader().load('globe.jpg')
+    }*/
+   map : new THREE.TextureLoader().load('globe.jpg')
   })
 )
 
